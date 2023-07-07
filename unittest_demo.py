9d9f7a26a84eaec1a22b6a3ebed063fccc7499ba
Lisha Zhou <lzhou1@ufl.edu>
2023-07-07 14:41:32 -0400

    add one more test for hex string

--- unittest_demo.py
+++ unittest_demo.py
@@ -6,8 +6,11 @@ from M4.numeric_conversion import *
 class NumberTest(unittest.TestCase):
     def test_hex_string_decode(self):
         hex_str = "0x2A"
-        # hex_str2 = "3B"
+        # add another test
+        hex_str2 = "3B"
         expected = 45
+        expected2 = 59
 
         result = hex_string_decode(hex_str)
         self.assertEqual(expected, result)
+        self.assertEqual(expected2, hex_string_decode(hex_str2))
