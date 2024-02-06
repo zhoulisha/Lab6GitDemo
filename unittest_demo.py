5b2854e1850dae35ce1dc44056a442ef2ff54d1b
zhoulisha <52050409+zhoulisha@users.noreply.github.com>
2024-02-06 10:47:44 -0500

    Update unittest_demo.py with function tests

--- unittest_demo.py
+++ unittest_demo.py
@@ -1,16 +1,8 @@
-import unittest
+def Add(a, b):
+        return a + b
+        
+def SayHello():
+        print("sup world from srcmake")
 
-from M4.numeric_conversion import *
-
-
-class NumberTest(unittest.TestCase):
-    def test_hex_string_decode(self):
-        hex_str = "0x2A"
-        # add another test
-        hex_str2 = "3B"
-        expected = 45
-        expected2 = 59
-
-        result = hex_string_decode(hex_str)
-        self.assertEqual(expected, result)
-        self.assertEqual(expected2, hex_string_decode(hex_str2))
+if __name__ == '__main__':
+        SayHello()
